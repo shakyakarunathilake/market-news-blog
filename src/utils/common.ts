740292e131b2a55088ad67574d404ded0e07f245
Shakya Karunathilake <46352201+shakyakarunathilake@users.noreply.github.com>
2025-08-09 19:38:34 +0530

    fix: update getBaseUrl function to prioritize URL environment variable

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -1,5 +1,5 @@
 export function getBaseUrl() {
   if (typeof window !== "undefined") return "";
 
-  return process.env.DEPLOY_URL ?? process.env.NEXT_PUBLIC_BASE_URL;
+  return process.env.URL ?? process.env.DEPLOY_URL ?? process.env.NEXT_PUBLIC_BASE_URL ;
 }
